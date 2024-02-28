296a1882a7d81da26a4a329dab915324a4e5df8b
David A. Hernandez II <djrhernandez@yahoo.com>
2024-02-28 13:10:49 -0800

    Mishap

--- app/dota2/page.tsx
+++ app/dota2/page.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useMemo, useState } from 'react'
-import { fetchApi } from './api/fetchApi'
-import { Charts } from './components/Charts'
-import { Table } from './components/Table'
+import { fetchApi } from '../api/fetchApi'
+import { Charts } from '../components/Charts'
+import { Table } from '../components/Table'
 
-import { capitalize } from './lib/utils'
-import * as searchStates from './lib/states'
+import { capitalize } from '../lib/utils'
+import * as searchStates from '../lib/states'
 
 export default function Dota2() {
 
