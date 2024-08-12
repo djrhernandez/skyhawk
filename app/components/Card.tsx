398c3a412cfc0a7a599447bdd959f5a5c8fb3c70
David A. Hernandez II <djrhernandez@yahoo.com>
2024-08-12 16:31:30 -0700

    Fixed card image logic

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Image from 'next/image'
 
-export const Card = ({ title, body, buttonText = null, image = null, onPrimaryClick = null, secondaryBtn = false, secondaryBody = null, onSecondaryClick = null, isMobile = false }) => {
+export const Card = ({ title, body, buttonText = null, image, onPrimaryClick = null, secondaryBtn = false, secondaryBody = null, onSecondaryClick = null, isMobile = false }) => {
 	return (
 		<div className="card">
 			<div className="card_header">
@@ -11,15 +11,17 @@ export const Card = ({ title, body, buttonText = null, image = null, onPrimaryCl
 
 			<div className='card_body'>
 				{/* TODO: Create space for image either below or next to text */}
-				<div className='card_image'>
-					<Image
-						src={image && image.src}
-						alt='Logo'
-						width={isMobile ? 96 : 128}
-						height={isMobile ? 96 : 128}
-						priority={true}
-					/>
-				</div>
+				{ image && (
+					<div className='card_image'>
+						<Image
+							src={image && image.src}
+							alt='Logo'
+							width={isMobile ? 96 : 128}
+							height={isMobile ? 96 : 128}
+							priority={true}
+						/>
+					</div>
+				)}
 
 				<div className="card_text">
 					<p>{body}</p>
